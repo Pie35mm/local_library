205c2eca0465b3870c4e0722d19d461520495f90
Pie35mm <82974026+Pie35mm@users.noreply.github.com>
2021-06-02 01:05:47 +0300

    Добавил в админ панель все модели, добавленные ранее.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
 # Register your models here.
